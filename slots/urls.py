30a37b817babbbea83c659f27b2232fcb66523d6
HARI-G-YADHAV <ambadileelakal@gmail.com>
2025-01-28 23:24:44 +0530

    stable

--- slots/urls.py
+++ slots/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.home_page, name='home'),
     path('login/', views.user_login, name='login'),
     path('registration/security', views.security_page, name='security'),
-    path('api/parking-spots/', views.get_parking_spots, name='parking-spots'),
+    path('api/parking-spots/', views.parking_spots_api, name='parking-spots'),
+    path('front/', views.front, name='front'),
 ]
