edfaff5a67db3f97d5e75a3fe1f7102638b073a0
akshaykkl <akkukkl10@gmail.com>
2025-01-29 23:09:12 +0530

    fixed

--- slots/admin.py
+++ slots/admin.py
@@ -3,7 +3,7 @@ from .models import ParkingSpot
 
 @admin.register(ParkingSpot)
 class ParkingSpotAdmin(admin.ModelAdmin):
-    list_display = ['name', 'latitude', 'longitude', 'status', 'created_at', 'updated_at']
-    list_filter = ['status']
-    search_fields = ['name']
-    ordering = ('id',)
+    list_display = ['number', 'name', 'vehicle_type', 'latitude', 'longitude', 'status', 'created_at', 'updated_at']
+    list_filter = ['status', 'vehicle_type']
+    search_fields = ['name', 'number']
+    ordering = ('number',)
